27aee35da770ba681a9d3bda134abca471614e25
Kaisma Penn-Titley <kptitley@gmail.com>
2017-07-19 05:46:18 -0400

    multiplication program

---
+++ README.md
@@ -0,0 +1,2 @@
+# cca-Intro2WebJS-2
+CCA Intro to Web Development - JavaScript, Print 1-100
---
+++ multiplication.js
@@ -0,0 +1,11 @@
+//Multiplication Tables Up to 12
+
+for (var i = 1; i <= 12; i++) {
+    console.log("The " + i + "'s Table");
+
+    for(var j = 0; j < 13; j++) {
+        console.log(i + " x " + j + " = " + i*j);
+    } if (i < 12) {
+        console.log("______________________________")
+    }
+}
